045c9326ddca4ce9931b4a5be47650b07c4be3ce
Andreas Hernández Hauser <andreashdez@gmail.com>
2024-03-06 21:53:39 +0100

    add updated configs

--- init.lua
+++ init.lua
@@ -808,6 +808,8 @@ require('lazy').setup({
         transparent = true,
       }
       vim.cmd.colorscheme 'tokyonight-night'
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
     end,
   },
 
