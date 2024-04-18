9aa08d84f24870532997451a52f4ee448f11eca9
Andreas Hernández Hauser <andreashdez@gmail.com>
2024-04-18 21:53:11 +0200

    add optional kickstart plugins

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -4,6 +4,15 @@ return {
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
     main = 'ibl',
-    opts = {},
+    opts = {
+      indent = {
+        char = '▏',
+        tab_char = { '▏' },
+      },
+      scope = {
+        show_start = false,
+        show_end = false,
+      },
+    },
   },
 }
