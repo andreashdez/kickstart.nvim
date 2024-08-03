2c1b26702a6a3f7cd0ccb00a12f3ea14eef1a22f
Andreas Hernández Hauser <andreashdez@gmail.com>
2024-08-03 18:00:34 +0200

    rearrange which-key entries

--- init.lua
+++ init.lua
@@ -278,11 +278,11 @@ require('lazy').setup({
       require('which-key').add {
         { '<leader>c', group = '[C]ode' },
         { '<leader>d', group = '[D]ocument' },
+        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
         { '<leader>r', group = '[R]ename' },
         { '<leader>s', group = '[S]earch' },
-        { '<leader>w', group = '[W]orkspace' },
         { '<leader>t', group = '[T]oggle' },
-        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        { '<leader>w', group = '[W]orkspace' },
       }
     end,
   },
