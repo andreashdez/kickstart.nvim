d370b2edde83bece93dc0c4adb488b66a290521c
Andreas Hernández Hauser <andreashdez@gmail.com>
2024-08-17 02:34:42 +0200

    remove usage of deprecated methods

--- lua/custom/plugins/evil_lualine.lua
+++ lua/custom/plugins/evil_lualine.lua
@@ -58,8 +58,8 @@ local evil_filetype = {
 
 local evil_lsp = {
   function()
-    local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
-    local clients = vim.lsp.get_active_clients()
+    local buf_ft = vim.api.nvim_get_option_value('filetype', {})
+    local clients = vim.lsp.get_clients()
     if next(clients) == nil then
       return ''
     end
