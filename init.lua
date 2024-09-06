54d0c36673e929cab15ed7e75f63891c7ef3078d
Andreas Hernández Hauser <andreashdez@gmail.com>
2024-09-07 01:14:03 +0200

    fix ts lsp warning

--- init.lua
+++ init.lua
@@ -1069,7 +1069,7 @@ lspconfig.rust_analyzer.setup {
 }
 lspconfig.pyright.setup {}
 lspconfig.taplo.setup {}
-lspconfig.tsserver.setup {}
+lspconfig.ts_ls.setup {}
 lspconfig.zls.setup {}
 
 -- The line beneath this is called `modeline`. See `:help modeline`
